004815501e98595d9ac6e1eda51026f07d3f9659
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-09-29 22:24:56 +0530

    Local

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8000/portfolio-website/';
+$config['base_url'] = 'http://localhost/portfolio-website/';
 
 /*
 |--------------------------------------------------------------------------
