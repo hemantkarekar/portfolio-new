7b787a3deffbb79f14ec58395396af7d2d3798e6
Hemant Karekar <hemant.v.karekar@gmail.com>
2023-10-15 23:12:39 +0530

    Config

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/portfolio-website/';
+$config['base_url'] = 'https://hemantkarekar.com/';
 
 /*
 |--------------------------------------------------------------------------
